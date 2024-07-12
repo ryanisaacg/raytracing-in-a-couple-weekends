9ee5242e2639a49d40c2e7589025f47e5d190f76
Ryan <ryanisaacg@users.noreply.github.com>
2024-07-12 16:33:11 -0400

    Chapter 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    fmt::Display,
+    ops::{Add, Div, Index, Mul, Sub},
+};
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
@@ -17,5 +22,106 @@ fn main() {
 
             println!("{} {} {}", ir, ig, ib);
         }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vector3 {
+    pub fn length(&self) -> f64 {
+        self.length2().sqrt()
+    }
+
+    // length2 = (self) => self.x * self.x + self.
+    pub fn length2(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn dot(&self, other: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+
+    pub fn cross(&self, other: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    pub fn normalize(self) -> Vector3 {
+        self / self.length()
+    }
+}
+
+impl Index<u32> for Vector3 {
+    type Output = f64;
+
+    fn index(&self, index: u32) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("out of bounds for vec3"),
+        }
+    }
+}
+
+impl Add<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Vector3) -> Self::Output {
+        Vector3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Sub<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: Vector3) -> Self::Output {
+        Vector3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vector3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl Div<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+impl Display for Vector3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
     }
 }
